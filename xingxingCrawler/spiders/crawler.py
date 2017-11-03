3040275448ce751014144d3e09782056136e5528
chengfei <chengfei001@gmail.com>
2017-11-03 15:34:43 +0800

    has bugs

--- xingxingCrawler/spiders/crawler.py
+++ xingxingCrawler/spiders/crawler.py
@@ -1,24 +1,26 @@
-# -*- coding: utf-8 -*-
 import scrapy
+from scrapy.selector import  Selector
 
-
-
-class CrawlerSpider(scrapy.Spider):
-    name = 'crawler'
-    def start_requests(self):
-        urls = ['http://www.ireadweek.com/index.php/index/2.html']
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+class QuotesSpider(scrapy.Spider):
+    name = "ireadweek"
+    start_urls = [
+        'http://www.ireadweek.com/index.php/index/2.html',
+    ]
 
     def parse(self, response):
-        self.log('hanghang %s' % response.css('ul.hanghang-list li.next a::attr(href)'))
+        print(response.css('ul.hanghang-list a::attr(href)'))
+        print('--------------------------------------')
+        for quote in response.css('ul.hanghang-list a::attr(href)'):
+            print('()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()')
+            print(quote.xpath('/a/@href').extract_first())
+            print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+            yield {
+                # 'text': quote.css('span.text::text').extract_first(),
+                # 'author': quote.xpath('span/small/text()').extract_first(),
+            }
 
+        # next_page = response.css('li.next a::attr("href")').extract_first()
+        # if next_page is not None:
+        #     yield response.follow(next_page, self.parse)
 
-        ''' 获取保存页面
-        page = response.url.split("/")[-2]
-        filename = '%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-        '''
 
