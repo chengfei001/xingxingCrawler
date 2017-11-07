1167675a0652c6baf74807402c522588272db75f
chengfei <chengfei001@gmail.com>
2017-11-07 16:36:05 +0800

    get url success

--- xingxingCrawler/spiders/crawler.py
+++ xingxingCrawler/spiders/crawler.py
@@ -8,19 +8,11 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        print(response.css('ul.hanghang-list a::attr(href)'))
-        print('--------------------------------------')
         for quote in response.css('ul.hanghang-list a::attr(href)'):
-            print('()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()')
-            print(quote.xpath('/a/@href').extract_first())
-            print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+            print(quote.extract())
             yield {
                 # 'text': quote.css('span.text::text').extract_first(),
                 # 'author': quote.xpath('span/small/text()').extract_first(),
             }
-
-        # next_page = response.css('li.next a::attr("href")').extract_first()
-        # if next_page is not None:
-        #     yield response.follow(next_page, self.parse)
 
 
